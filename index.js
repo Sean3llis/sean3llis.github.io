07c9efa2046abc8e6a3aa8708df5a0b7399cabf4
Sean Ellis <seane235@gmail.com>
2014-09-08 18:23:38 -0400

    implemented main function

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-$(document).ready(function() {
+var main = function() {
 	$('.titlebar').mouseenter(function(){
 		$(this).animate({
-			height: '+=150px'
+			height: '+=100px'
 		});
 	});
 	$('.titlebar').mouseleave(function() {
 		$(this).animate({
-			height: '-=150px'
+			height: '-=100px'
 		});
 	});
-});
+};
+
+$(document).ready(main);
