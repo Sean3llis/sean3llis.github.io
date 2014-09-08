45155f3e8c1e8d9352675a92718e335b7069cfc7
Sean Ellis <seane235@gmail.com>
2014-09-08 18:43:18 -0400

    added box main animation

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var main = function() {
 	$('.titlebar').mouseenter(function(){
 		$(this).animate({
-			height: '+=100px'
+			height: '+=65px'
 		});
 	});
 	$('.titlebar').mouseleave(function() {
 		$(this).animate({
-			height: '-=100px'
+			height: '-=65px'
+		});
+	});
+	$('.boxmain').mouseenter(function(){
+		$(this).animate({
+			height: '+=5px'
+		});
+	});
+	$('.boxmain').mouseleave(function() {
+		$(this).animate({
+			height: '-=5px'
 		});
 	});
 };
